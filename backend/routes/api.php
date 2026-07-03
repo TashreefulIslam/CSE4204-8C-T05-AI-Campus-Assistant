807943e27ac81e1d5d9160585d9ce93be3501d4e
Tashreeful Islam <tashreeful12@gmail.com>
2026-07-03 23:58:36 +0600

    Add user management routes to API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,10 +7,12 @@ use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\AssignmentController;
 use App\Http\Controllers\Api\NoticeController;
 use App\Http\Controllers\Api\AiRequestController;
+use App\Http\Controllers\Api\UserController;
 
 // Authentication Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::put('/update-password', [AuthController::class, 'updatePassword'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/profile', [AuthController::class, 'profile']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->apiResource('courses', CourseController::class);
@@ -20,3 +22,13 @@ Route::middleware('auth:sanctum')->apiResource('assignments', AssignmentControll
 Route::middleware('auth:sanctum')->apiResource('notices', NoticeController::class);
 
 Route::middleware('auth:sanctum')->apiResource('ai-requests', AiRequestController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // User Management
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+});
