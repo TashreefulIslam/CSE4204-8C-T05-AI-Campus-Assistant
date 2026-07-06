f22476d44db428b133ba478edc440070bfea98bf
mirajurrhman175 <mirajurrahman175@gmail.com>
2026-07-07 01:39:18 +0800

    Add departments data to departments.js

---
+++ frontend/srs/data/departments.js
@@ -0,0 +1,7 @@
+const departments = [
+  "Computer Science & Engineering (CSE)",
+  "Electrical & Electronic Engineering (EEE)",
+  "Business Administration (BBA)",
+];
+
+export default departments;
